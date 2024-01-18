defc309b0e3f172f3ca98f07167f808b24eeede4
Oleg <oleg.bortovsky@gmail.com>
2024-01-18 09:00:54 +0200

    Global commit 1/18/2024

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['primereact/multiSelect', 'primereact/inputText'],
+      external: ['primereact/multiSelect', 'primereact/inputText', 'primereact/inputNumber'],
     },
   },
 })
