724a3cd1fc9cffd9ecf6003ebba509265ebdb8d5
Oleg <oleg.bortovsky@gmail.com>
2024-01-18 10:51:33 +0200

    Add to vite external: 'primereact/multiSelect', 'primereact/inputText', 'inputNumber'

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['primereact/multiSelect', 'primereact/inputText', 'inputNumber'],
+      external: ['primereact/multiSelect', 'primereact/inputText', 'primereact/inputNumber'],
     },
   },
 })
